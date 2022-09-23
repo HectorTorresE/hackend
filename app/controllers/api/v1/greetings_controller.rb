76769bd405672519781abe396fa3830be1404791
HectorTorresE <hectorjte@gmail.com>
2022-09-22 19:38:07 -0600

    Fix linter

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,5 +1,11 @@
-class Api::V1::GreetingsController < ApplicationController
-  def index
-    render json: Greeting.offset(Random.rand(0..4)).first.message
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class GreetingsController < ApplicationController
+      def index
+        render json: Greeting.offset(Random.rand(0..4)).first.message
+      end
+    end
   end
 end
