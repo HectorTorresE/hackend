6fee89e0dcfae4c4ef12b6d1e7c1a4fbb4348941
Hector Torres <hectorjte@gmail.com>
2022-09-22 19:47:49 -0600

    Revert "Setup"

--- app/controllers/api/v1/greetings_controller.rb
+++
@@ -1,9 +0,0 @@
-module Api
-  module V1
-    class GreetingsController < ApplicationController
-      def index
-        render json: Greeting.offset(Random.rand(0..4)).first.message
-      end
-    end
-  end
-end
